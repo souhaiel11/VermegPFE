d4fe7acfcb023dc975426f2a18e2dc3b5dfbf654
Souhaiel Amri <souhaiel.amri@esprit.tn>
2025-05-17 01:44:14 +0100

    sprin test

--- src/test/java/tn/esprit/spring/BlocServiceMockTest.java
+++ src/test/java/tn/esprit/spring/BlocServiceMockTest.java
@@ -1,47 +1,52 @@
 package tn.esprit.spring;
 
-
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
-@TestMethodOrder(MethodOrderer.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 public class BlocServiceMockTest {
 
     @BeforeEach
     void beforeEach() {
-
+        System.out.println("Before each test");
     }
 
     @AfterEach
     void afterEach() {
-
+        System.out.println("After each test");
     }
 
     @Order(1)
     @RepeatedTest(4)
     void test() {
-
-    }
-
-    @Order(4)
-    @Test
-    void test2() {
-
+        int expected = 2 + 2;
+        assertEquals(4, expected, "Basic math test failed");
     }
 
     @Order(2)
     @Test
     void test3() {
-
+        String name = "Bloc A";
+        assertTrue(name.startsWith("Bloc"), "Name should start with 'Bloc'");
     }
 
     @Order(3)
     @Test
     void test4() {
+        Object obj = new Object();
+        assertNotNull(obj, "Object should not be null");
+    }
 
+    @Order(4)
+    @Test
+    void test2() {
+        boolean available = true;
+        assertTrue(available, "Expected 'available' to be true");
     }
 }
